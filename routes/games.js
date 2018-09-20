de8f57b1c7335f7408a019a853397de30036d16b
miknhoj <kimjohan86@gmail.com>
2018-09-20 10:26:26 -0400

    added create route - shows new game that is made - wouldn't work with players field

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { Game, Player } = require('../db/schema')
+const { Game } = require('../db/schema')
 
 // INDEX , SHOW ALL
 router.get('/', (req, res) => {
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
       res.render('games/index', {games})
   })
 });
-
 
 // NEW, RENDER NEW FORM
 router.get('/new', (req,res) => {
@@ -27,6 +26,12 @@ router.get('/:id', (req, res) => {
 // EDIT, RENDER EDIT FORM
 
 // CREATE
+router.post('/', (req, res) => {
+  Game.create(req.body)
+    .then((game) => {
+      res.redirect(`/games/${game._id}`)
+    })
+})
 
 // UPDATE
 
