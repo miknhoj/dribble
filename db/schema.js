ccfd34ef9bfe4114595fadd53164805c0223c60c
miknhoj <kimjohan86@gmail.com>
2018-09-19 23:29:29 -0400

    updated schema file

---
+++ db/schema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const StatSchema = new Schema ({
+    points: Number,
+    rebounds: Number,
+    assists: Number,
+    steals: Number,
+})
+
+const PlayerSchema = new Schema ({
+    name: String,
+    stats: [StatSchema],
+})
+
+const GameSchema = new Schema ({
+    date: Number,
+    location: String,
+    gameNumber: Number,
+    players: [PlayerSchema]
+})
+
+const StatModel = mongoose.model('Stat', StatSchema)
+const PlayerModel = mongoose.model('Player', PlayerSchema)
+const GameModel = mongoose.model('Game', GameSchema)
+
+module.exports = {
+    Stat: StatModel,
+    Player: PlayerModel,
+    Game: GameModel
+}
